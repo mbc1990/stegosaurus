92662a978fd1ad7d0d805c6db639f0a8da2596e8
Malcolm <malcolmbalch@gmail.com>
2020-06-23 00:40:44 -0400

    Re-encode jpg, bump version to fix image darkening

--- stegosaurus/src/main.rs
+++ stegosaurus/src/main.rs
@@ -3,7 +3,6 @@ extern crate image;
 use image::{GenericImageView, GenericImage};
 use image::jpeg::JPEGEncoder;
 use std::fs::File;
-use image::png::PNGEncoder;
 /*
 Unobtrusive uniquely identifying watermark that is tolerant to cropping, resizing, compression/reencoding, and potentially other transformations (such as shear) and not easily recognizable by a human eye.
     - Modify pixel values slightly in unique pattern across whole image (checkerboard for example)
@@ -11,8 +10,33 @@ Unobtrusive uniquely identifying watermark that is tolerant to cropping, resizin
         - For example, r, g, b are all prime numbers
 */
 fn main() {
-    let img = image::open(String::from("/home/malcolm/projects/stegosaurus/stegosaurus/whitesquare.jpeg")).unwrap();
-    let mut writer = File::create("/home/malcolm/projects/stegosaurus/stegosaurus/whitesquare_reencoded-release22.jpeg").unwrap();
-    let mut encoder = JPEGEncoder::new_with_quality(&mut writer, 100);
-    encoder.encode_image(&img);
+    println!("Hello, world!");
+    let img = image::open(String::from("/home/malcolm/projects/stegosaurus/stegosaurus/kitten.jpg"));
+    if let Ok(mut di) = img {
+        println!("Read an image");
+        let (width, height) = di.dimensions();
+        for w in 0..width {
+            for h in 0..height {
+                /*
+                let mut px = di.get_pixel(w, h);
+                let (red, green, blue, alpha)  = (px.0[0], px.0[1], px.0[2], px.0[3]);
+                let mut red_new = red;
+                if false  {
+                    red_new = (red + 1) % 255;
+                    println!("Old red: {:}, new red: {:}", red, red_new);
+                }
+
+                let new_px = image::Rgba([red_new, green, blue, alpha]);
+                di.put_pixel(w, h, new_px);
+                */
+            }
+        }
+
+        // di.save(String::from("/home/malcolm/projects/stegosaurus/stegosaurus/testimg1-unmodified.jpg"));
+
+        // TODO: Doesn't work - need to see if encoding with 100 quality prevents output appearing dark
+        let mut writer = File::create("/home/malcolm/projects/stegosaurus/stegosaurus/out-kitten.jpg").unwrap();
+        let mut encoder = JPEGEncoder::new_with_quality(&mut writer, 100);
+        encoder.encode_image(&di);
+    }
 }
